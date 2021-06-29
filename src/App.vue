41722a5eeb8e2bbc28eb1ae3d662515f2f877d35
jade-liu <2259023634@qq.com>
2021-06-29 17:24:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="page-component__scroll">
+    <el-scrollbar class="scrollbar">
+      <div id="nav" class="nav-class">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
+    </el-scrollbar>
+    <el-backtop
+      target=".page-component__scroll .el-scrollbar__wrap"
+      :bottom="100"
+      :visibility-height="50"
+    ></el-backtop>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.scrollbar {
+  height: calc(100vh - 60px);
+}
+/deep/.el-scrollbar__wrap {
+  overflow-x: hidden;
+}
+</style>
